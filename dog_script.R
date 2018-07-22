b03597ee24956e6b5df0612fa46deeb9332498b7
elansparsons <elansparsons@gmail.com>
2018-07-22 13:55:11 -0400

    more summary analysis of 2017 Allegheny County including breeds, sex

--- dog_script.R
+++ dog_script.R
@@ -11,7 +11,7 @@ data(zipcode)
 
 #combine datasets with an inner join
 locs <- merge(dogs_2017,zipcode,by.x="OwnerZip",by.y="zip")
-locs <- unique(locs)
+locs <- unique(locs) #remove duplicated entries
 
 cities <- as.data.frame(sort(table(locs$city), decreasing=T))
 zips <- as.data.frame(sort(table(locs$OwnerZip), decreasing=T))
@@ -23,16 +23,31 @@ pitt <- locs[locs$city=="Pittsburgh",]
 sum(cities$count[cities$city != "Pittsburgh"])
 cities$count[cities$city == "Pittsburgh"]/sum(cities$count)
 
-#which breeds are most common?
-labs <- locs[grepl("LAB",locs$Breed),]
-retrs <- locs[grepl("RETRIEVER",locs$Breed),]
-shepherds <- locs[grepl("SHEP",locs$Breed),]
-poodles <- locs[grepl("OODLE",locs$Breed),]
-terriers <- locs[grepl("TERRIER",locs$Breed),]
+#which breeds are most common? In county
+labs <- locs[grepl("LAB",locs$Breed),] #2
+retrs <- locs[grepl("RETRIEV",locs$Breed),] #4
+shepherds <- locs[grepl("SHEP",locs$Breed),] #5
+poodles <- locs[grepl("OODLE",locs$Breed),] 
+terriers <- locs[grepl("TERR",locs$Breed),] #1
 pitbulls <- locs[grepl("PIT BULL",locs$Breed),]
-mixed <- locs[grepl("MIXED",locs$Breed,fixed=TRUE),]
+mixed <- locs[grepl("MIXED",locs$Breed,fixed=TRUE),] #3
+
 
 others <- locs[!(locs$Breed %in% c(labs$Breed,shepherds$Breed,poodles$Breed,terriers$Breed,pitbulls$Breed,retrs$Breed,mixed$Breed)),]
+otherssort <- as.data.frame(sort(table(others$Breed), decreasing=T))
+
+#breeds for pittsburgh only
+pittlab <- labs[labs$city=="Pittsburgh",] #2
+pittretrs <- retrs[retrs$city=="Pittsburgh",] #4
+pittshep <- shepherds[shepherds$city=="Pittsburgh",] #5
+pittpoodles <- poodles[poodles$city=="Pittsburgh",]
+pittterr <- terriers[terriers$city=="Pittsburgh",] #1
+pittbulls<- pitbulls[pitbulls$city=="Pittsburgh",]
+pittmixed <- mixed[mixed$city=="Pittsburgh",] #3
+
+pittothers <- others[others$city=="Pittsburgh",]
+pittotherssort <- as.data.frame(sort(table(pittothers$Breed), decreasing=T))
+
 
 #are there common names?
 names <- as.data.frame(sort(table(locs$DogName), decreasing=T))
@@ -51,4 +66,7 @@ sum(grepl("Female",locs$LicenseType),na.rm=TRUE)/nrow(locs)
 sum(grepl("Male",locs$LicenseType),na.rm=TRUE)/nrow(locs)
 
 sum(grepl("Female",pitt$LicenseType),na.rm=TRUE)/nrow(pitt)
-sum(grepl("Male",pitt$LicenseType),na.rm=TRUE)/nrow(pitt)
+sum(grepl("Male",pitt$LicenseType),na.rm=TRUE)/nrow(pitt) #sexes are roughly even in county and Pittsburgh
+
+
+#import combined dataset
